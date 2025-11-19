117a7fd1ce311882d4f561d240ec74b797ac71e1
odilia-a <osiosesharon@gmail.com>
2025-11-19 23:33:22 +0100

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const connectDB = async () => {
+
+    try {
+        await mongoose.connect(process.env.URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }   
+};
+
+module.exports = connectDB;
